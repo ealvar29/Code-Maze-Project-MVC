d2d8680207804ae44a6fe197f26dbef882993d95
Eduardo <edalvarez2015@gmail.com>
2020-12-10 23:27:11 -0600

    Reading documentation on how controllers work

---
+++ BookStore/BookStore/Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET
+        public string Index()
+        {
+            return "This is the book index.";
+        }
+
+        public string Details()
+        {
+            return "This is the details of a book.";
+        }
+    }
+}
